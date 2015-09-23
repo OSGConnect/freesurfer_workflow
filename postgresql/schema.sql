f94389a8a5548b5aa8a90a44303a6b0b88b0d9ea
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-09-23 15:30:15 -0500

    Add schema for DB

---
+++ postgresql/schema.sql
@@ -0,0 +1,34 @@
+BEGIN;
+
+CREATE SCHEMA freesurfer_interface;
+
+CREATE TYPE freesurfer_interface.job_state AS ENUM (
+    'UPLOADED',
+    'PROCESSING',
+    'FAILED',
+    'COMPLETED'
+);
+
+CREATE TABLE freesurfer_interface.users (
+    id              SERIAL PRIMARY KEY,
+    username        varchar(128) NOT NULL UNIQUE CHECK ( username <> ''),
+    first_name      varchar(128) NOT NULL CHECK ( first_name <> ''),
+    last_name       varchar(128) NOT NULL CHECK ( last_name <> ''),
+    email           varchar(128) NOT NULL CHECK ( email <> ''),
+    institution     varchar(128) NOT NULL CHECK ( institution <> ''),
+    phone           varchar(128) NOT NULL CHECK ( phone <> ''),
+    password        char(64) NOT NULL CHECK ( password <> ''),
+    salt            char(64) NOT NULL CHECK ( salt <> '')
+);
+
+CREATE TABLE freesurfer_interface.jobs (
+    id              SERIAL PRIMARY KEY,
+    user_id         varchar(128) NOT NULL REFERENCES freesurfer_interface.users(username),
+    image_filename  varchar(128) NOT NULL,
+    log_filename    varchar(128) NOT NULL,
+    state           freesurfer_interface.job_state NOT NULL,
+    job_date        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    purged          boolean NOT NULL DEFAULT FALSE
+);
+
+COMMIT;
