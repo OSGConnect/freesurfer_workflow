d51004e72b465028569489fe3e0547ef484cfa63
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-10-28 10:09:11 -0500

    A few final fixes for test file

--- wsgi/freesurfer_test.py
+++ wsgi/freesurfer_test.py
@@ -11,23 +11,6 @@ import random
 TIMEZONE = "US/Central"
 
 
-def get_db_parameters():
-    """
-    Read database parameters from a file and return it
-
-    :return: a tuple of (database_name, user, password, hostname)
-    """
-    parameters = {}
-    with open(PARAM_FILE_LOCATION) as param_file:
-        line = param_file.readline()
-        key, val = line.strip().split('=')
-        parameters[key.strip()] = val.strip()
-    return (parameters['database'],
-            parameters['user'],
-            parameters['password'],
-            parameters['hostname'])
-
-
 def publish_record(record, channel, redis_client):
     """
     Publishes a record to a Redis pub/sub channel
@@ -52,7 +35,7 @@ def delete_job(environ):
     """
     response_body = "{ \"status\": 200,\n \"result\": \"success\" }"
     status = '200 OK'
-    if (random.random() > 0.9):
+    if random.random() > 0.9:
         # give an error in 10% of the cases
         return "{ \"status\": 500,\n \"result\": \"Server Error\" }", '500 Server Error'
     return response_body, status
@@ -134,6 +117,7 @@ def submit_job(environ):
     return "{ \"status\": 200,\n \"result\": \"success\" }", '200 OK'
 
 
+
 def application(environ, start_response):
     """
     Get parameters from GET request and publish to redis channel
@@ -156,6 +140,9 @@ def application(environ, start_response):
         response_body, status = submit_job(environ)
     elif environ['REQUEST_METHOD'] == 'DELETE':
         response_body, status = delete_job(environ)
+    else:
+        response_body = '500 Server Error'
+        status = '500 Server Error'
 
     response_headers = [('Content-Type', 'text/html'),
                         ('Content-Length', str(len(response_body)))]
@@ -168,8 +155,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Parse request and act appropriately')
     parser.add_argument('--host', dest='hostname', default=socket.getfqdn(),
                         help='hostname of server')
-    parser.add_argument('--dbparams', dest='db_param_file', default=PARAM_FILE_LOCATION,
-                        help='location of file with database information')
     args = parser.parse_args(sys.argv[1:])
     srv = make_server(args.hostname, 8080, application)
     srv.serve_forever()
