f0be818a85e18649833a63a3ea845274e63fdd89
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-12-29 11:19:37 -0600

    Use __all__ in __init__ instead of importing everything

--- python/fsurfer/__init__.py
+++ python/fsurfer/__init__.py
@@ -1,3 +1,8 @@
 
-from fsurfer import *
+__all__ = ['create_single_job',
+           'create_recon2_job',
+           'create_initial_job',
+           'create_hemi_job',
+           'create_final_job']
+
 
