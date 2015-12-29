5b2f77752f02d8f49807807524835667ec32e7cb
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-12-28 18:35:00 -0600

    Use correct library name and include config in rpm

--- python/setup.py
+++ python/setup.py
@@ -9,13 +9,13 @@ from distutils.core import setup
 
 SCRIPT_PATH = os.path.abspath('../bash')
 setup(name='fsurf',
-      version='0.1',
+      version='0.2',
       description='Command line tool to submit freesurfer workflows',
       author='Suchandra Thapa',
       author_email='sthapa@ci.uchicago.edu',
       url='https://github.com/OSGConnect/freesurfer_workflow',
       packages=['fsurfer'],
-      scripts=['fsurf-osgconnect'],
+      scripts=['fsurf-osgconnect', 'fsurf-config'],
       data_files=[('/usr/share/fsurfer/scripts', [os.path.join(SCRIPT_PATH,
                                                                'autorecon1.sh'),
                                                   os.path.join(SCRIPT_PATH,
