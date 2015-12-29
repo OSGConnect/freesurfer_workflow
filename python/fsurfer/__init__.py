4b9fa50c4c2d1024628730afd10115e74390c816
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-12-29 11:27:05 -0600

    More package changes

--- python/fsurfer/__init__.py
+++ python/fsurfer/__init__.py
@@ -5,4 +5,8 @@ __all__ = ['create_single_job',
            'create_hemi_job',
            'create_final_job']
 
-
+from fsurfer import create_single_job
+from fsurfer import create_recon2_job
+from fsurfer import create_initial_job
+from fsurfer import create_hemi_job
+from fsurfer import create_final_job
