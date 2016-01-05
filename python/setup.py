6551fa1a89403cb9cfff7f068944d1766abac092
Suchandra Thapa <sthapa@ci.uchicago.edu>
2016-01-05 11:11:58 -0600

    Make sure to specify the output directory

--- python/setup.py
+++ python/setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 
 setup(name='fsurf',
-      version='0.5.1',
+      version='0.5.3',
       description='Command line tool to submit freesurfer workflows',
       author='Suchandra Thapa',
       author_email='sthapa@ci.uchicago.edu',
