88b69c231af4494923e0eab11b9815d41dc91f73
Suchandra Thapa <sthapa@ci.uchicago.edu>
2016-01-05 11:12:29 -0600

    Add some output and make sure to copy rpms back

--- python/create_package.sh
+++ python/create_package.sh
@@ -2,9 +2,12 @@
 
 work_dir=`mktemp -d`
 cur_dir=$PWD
+echo "Scratch dir: $work_dir"
 cp -a * $work_dir
 cp -a ../bash $work_dir
+"Generating package"
 cd $work_dir
 mv fsurf-osgconnect fsurf
 python setup.py bdist_rpm
+cp dist/*.rpm $cur_dir
 rm -fr $work_dir
