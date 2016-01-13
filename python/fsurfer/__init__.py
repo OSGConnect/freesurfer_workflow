7156d040620bd029cccba5c776aa3e53e9b9efaf
Suchandra Thapa <sthapa@ci.uchicago.edu>
2016-01-13 12:28:48 -0600

    Add minor fixes

--- python/fsurfer/__init__.py
+++ python/fsurfer/__init__.py
@@ -7,3 +7,12 @@ __all__ = ['create_single_job',
            'create_serial_workflow',
            'create_diamond_workflow',
            'create_single_workflow']
+
+from fsurfer import create_single_job
+from fsurfer import create_recon2_job
+from fsurfer import create_initial_job
+from fsurfer import create_hemi_job
+from fsurfer import create_final_job
+from fsurfer import create_serial_workflow
+from fsurfer import create_diamond_workflow
+from fsurfer import create_single_workflow
