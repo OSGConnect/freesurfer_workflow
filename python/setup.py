11582dfaf376d33b6d7b7dcec939e810b12a804e
Suchandra Thapa <sthapa@ci.uchicago.edu>
2016-01-15 11:25:01 -0600

    Add version info to files

--- python/setup.py
+++ python/setup.py
@@ -2,12 +2,13 @@
 
 # Copyright 2015 University of Chicago
 # Available under Apache 2.0 License
-
+# setup for fsurf on OSG Connect login
 from distutils.core import setup
 
+from fsurf import VERSION
 
 setup(name='fsurf',
-      version='0.6.1',
+      version=VERSION,
       description='Command line tool to submit freesurfer workflows',
       author='Suchandra Thapa',
       author_email='sthapa@ci.uchicago.edu',
