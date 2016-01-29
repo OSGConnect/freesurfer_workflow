de429d4b9880ccb5557e971f795b4ae9aed12794
Suchandra Thapa <sthapa@ci.uchicago.edu>
2016-01-28 18:04:19 -0600

    Add fixes for package generation, clean the MANIFEST file and do builds in temp dir

--- python/create_fsurfer_package.sh
+++ python/create_fsurfer_package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# create fsurfer lib rpms
 work_dir=`mktemp -d`
 cur_dir=$PWD
 echo "Scratch dir: $work_dir"
@@ -7,7 +8,35 @@ cp -a * $work_dir
 cp -a ../bash $work_dir
 "Generating package"
 cd $work_dir
+rm MANIFEST
+python setup_fsurfer.py bdist_rpm
+cp dist/*.rpm $cur_dir
+rm -fr $work_dir
+
+# create fsurfer script rpms
+cd $cur_dir
+work_dir=`mktemp -d`
+echo "Scratch dir: $work_dir"
+cp -a * $work_dir
+cp -a ../bash $work_dir
+"Generating package"
+cd $work_dir
+rm MANIFEST
 mv fsurf-osgconnect fsurf
-python setup.py bdist_rpm
+python setup_osgconnect.py bdist_rpm
+cp dist/*.rpm $cur_dir
+rm -fr $work_dir
+
+# create fsurfer script rpms
+cd $cur_dir
+work_dir=`mktemp -d`
+echo "Scratch dir: $work_dir"
+cp -a * $work_dir
+cp -a ../bash $work_dir
+"Generating package"
+cd $work_dir
+rm MANIFEST
+mv fsurf-osgconnect fsurf
+python setup_fsurfer_backend.py bdist_rpm
 cp dist/*.rpm $cur_dir
 rm -fr $work_dir
