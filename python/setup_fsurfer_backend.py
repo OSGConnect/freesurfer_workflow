5408ced0ba5f459827c2db755b060c1614222841
Suchandra Thapa <sthapa@ci.uchicago.edu>
2016-09-06 02:04:55 -0500

    Fix constant def

--- python/setup_fsurfer_backend.py
+++ python/setup_fsurfer_backend.py
@@ -6,7 +6,7 @@
 from distutils.core import setup
 
 setup(name='fsurfer-backend',
-      version='VERSION',
+      version='PKG_VERSION',
       description='Scripts to handle background freesurfer processing',
       author='Suchandra Thapa',
       author_email='sthapa@ci.uchicago.edu',
